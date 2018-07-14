ad262122b5afe5047ad92fe9f0b93d06fda89b14
Brian Bycynski <high5bri@hotmail.com>
2018-07-14 10:52:58 -0400

    finished lecture 169

--- client/src/containers/home_container.jsx
+++ client/src/containers/home_container.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getBooks } from '../actions';
+
+import BookItem from '../widgetsUI/book_item';
+
 class HomeContainer extends Component {
     componentWillMount() {
-        this.props.dispatch(getBooks(6, 0, 'desc'));
+        this.props.dispatch(getBooks(1, 0, 'desc'));
     }
 
-    renderItems = books => (books.list ? books.list.map(item => 'item') : null);
+    renderItems = books =>
+        books.list
+            ? books.list.map(item => <BookItem {...item} key={item._id} />)
+            : null;
+
+    loadmore = () => {
+        let count = this.props.books.list.length;
+        this.props.dispatch(getBooks(1, count, 'desc', this.props.books.list));
+    };
 
     render() {
-        return <div>{this.renderItems(this.props.books)}</div>;
+        return (
+            <div>
+                {this.renderItems(this.props.books)}
+                <div className="loadmore" onClick={this.loadmore}>
+                    Load More
+                </div>
+            </div>
+        );
     }
 }
 
